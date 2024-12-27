c918ca0074a92bd47891c4435f6a3d679c1ebe8c
YoungAhBin <YoungAhBin@outlook.com>
2024-12-27 17:49:43 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+
+import sys
+from PySide6 import QtWidgets
+from MyPySide6.Main_interface import MyWidget
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWidget()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
